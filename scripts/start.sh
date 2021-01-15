a706ddc742a6af4e5790db6950a2d372d53e0daa
pokamest <pokamest@gmail.com>
2021-01-16 00:44:05 +0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,23 +6,23 @@ mkdir -p /dev/net
 
 if [ ! -c /dev/net/tun ]; then
     echo "$(datef) Creating tun/tap device."
-    sudo mknod /dev/net/tun c 10 200
+    mknod /dev/net/tun c 10 200
 fi
 
 # Allow UDP traffic on port 1194.
-sudo iptables -A INPUT -i eth0 -p udp -m state --state NEW,ESTABLISHED --dport 1194 -j ACCEPT
-sudo iptables -A OUTPUT -o eth0 -p udp -m state --state ESTABLISHED --sport 1194 -j ACCEPT
+iptables -A INPUT -i eth0 -p udp -m state --state NEW,ESTABLISHED --dport 1194 -j ACCEPT
+iptables -A OUTPUT -o eth0 -p udp -m state --state ESTABLISHED --sport 1194 -j ACCEPT
 
 # Allow traffic on the TUN interface.
-sudo iptables -A INPUT -i tun0 -j ACCEPT
-sudo iptables -A FORWARD -i tun0 -j ACCEPT
-sudo iptables -A OUTPUT -o tun0 -j ACCEPT
+iptables -A INPUT -i tun0 -j ACCEPT
+iptables -A FORWARD -i tun0 -j ACCEPT
+iptables -A OUTPUT -o tun0 -j ACCEPT
 
 # Allow forwarding traffic only from the VPN.
-sudo iptables -A FORWARD -i tun0 -o eth0 -s 10.8.0.0/24 -j ACCEPT
-sudo iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -i tun0 -o eth0 -s 10.8.0.0/24 -j ACCEPT
+iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-sudo iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
 
 # Aute start openvpn if it configured
 if [ -f /etc/openvpn/ca.crt ]; then (openvpn --config /etc/openvpn/server.conf &); fi
